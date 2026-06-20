b2b10cca956d52e7dca1adb64811593a37f5d703
mvrmiranda <marcos.miranda.ext@outlook.com>
2026-06-20 11:08:04 -0300

    feat: implement basic standard input reading loop

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h> /* API Posix standard */
+
+int main() {
+    char c;
+
+    // STDIN_FILENO: É uma constante que representa o File Descriptor
+    // (Descritor de Arquivo) número 0.
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c) /* Avalia se o byte lido é imprìmivel */) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
+
+    return 0;
+}
